4b3bd289a2334651e39a59490a1622133c05d36b
stefanKnott <stkn5801@colorado.edu>
2016-01-27 14:11:44 -0700

    Full schema, validation function added

--- app/models/rides.server.model.js
+++ app/models/rides.server.model.js
@@ -5,9 +5,60 @@
 var mongoose = require('mongoose'),
 Schema = mongoose.Schema,
 crypto = require('crypto');
+
+//possible validations:
+var validateCost = function(cost){
+	return (cost >= 0.00);
+}
+
+
 /**
-* A Validation function for local strategy properties
+* Ride Schema
 */
-var validateLocalStrategyProperty = function(property) {
-return ((this.provider !== 'local' && !this.updated) || property.length);
-};
+
+var RideSchema = new Schema({
+	requester: @user_db_reference,
+	ride_state: {
+		type: String,
+		trim: true,
+		default: ''
+	},
+	ride_est: {	//Estimate in USD, greater than or equal to 0
+		type: Number,
+		default:'0.00',
+		validate: [validateCost, 'Cost cannot be below 0.00']
+	},
+	current_route:{
+		type: Array[(Number, Number)]
+	},
+	updated:{
+		type: Date
+	},
+	//relevant_uber_info,
+	ride_users:[{
+		user_id: @user_db_reference,
+		location:{
+			type: (Number, Number),
+		}
+		phone:
+			type: String,
+			trim: true,
+			default: '555-555-5555'
+		}
+		payment_status: { //possible statuses: paid/unpaid
+			type: String,
+			trim: true
+			default: 'unpaid'
+		}
+		amount:{ //greater than or equal to 0
+			type: Number,
+			default: '0.00',
+			validate: [validateCost, 'Cost cannot be below 0.00']
+		}
+		accept_stats:{ //possible statuses: yes/no
+			type: String,
+			trim: true,
+			default: "no"
+		}
+	}]
+})
